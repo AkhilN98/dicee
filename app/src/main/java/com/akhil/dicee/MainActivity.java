8a148103b6deb965f0ede5ae87c2b9193d370789
Akhil <akhil.techgeek@gmail.com>
2019-06-09 20:21:22 +0530

    Version 1.1: Added Functionality: Button changes Dice Randomly.

--- app/src/main/java/com/akhil/dicee/MainActivity.java
+++ app/src/main/java/com/akhil/dicee/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,13 +20,36 @@ public class MainActivity extends AppCompatActivity {
 
         Button rollButton = (Button) findViewById(R.id.rollButton);
 
-        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
 
-        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+        final int[] diceImg = {R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
 
         rollButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Log.d("Dicee","This is a app");
+
+                Random rng = new Random();
+
+                int randNum = rng.nextInt(6);
+
+                int randNum2 = rng.nextInt(6);
+
+                Log.d("Dicee", "Random number is "+ randNum);
+
+                Log.d("Dicee", "Random number 2 is "+ randNum2);
+
+                leftDice.setImageResource(diceImg[randNum]);
+
+                rightDice.setImageResource(diceImg[randNum2]);
+
             }
         });
     }
